b7d0f848a95b44c78ce007192acfcbafebae3873
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-09-30 13:35:00 +0100

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,14 @@
+const createError = require('http-errors');
+
 export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
-  res.status(status).json({ message: err.message || "Internal Server Error" });
+ 
+  if (err instanceof createError.HttpError) {
+   
+    const status = err.status || 500;
+    res.status(status).json({ message: err.message });
+  } else {
+    
+    const status = err.status || 500;
+    res.status(status).json({ message: err.message || "Internal Server Error" });
+  }
 };
