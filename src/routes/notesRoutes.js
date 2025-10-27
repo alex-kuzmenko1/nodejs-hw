e4fcdfe8469a28cc4ab360195ce9f2f1c40b58a6
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-10-27 15:21:55 +0000

    ch

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -16,23 +16,14 @@ import {
 
 const router = Router();
 
+router.get("/notes", celebrate(getAllNotesSchema), getAllNotes);
 
-router.get("/", celebrate(getAllNotesSchema), getAllNotes);
+router.get("/notes/:noteId", celebrate(noteIdSchema), getNoteById);
 
+router.post("/notes", celebrate(createNoteSchema), createNote);
 
-router.get("/:noteId", celebrate(noteIdSchema), getNoteById);
+router.delete("/notes/:noteId", celebrate(noteIdSchema), deleteNote);
 
-
-router.post("/", celebrate(createNoteSchema), createNote);
-
-
-router.delete("/:noteId", celebrate(noteIdSchema), deleteNote);
-
-
-router.patch(
-  "/:noteId",
-  celebrate(updateNoteSchema),
-  updateNote
-);
+router.patch("/notes/:noteId", celebrate(updateNoteSchema), updateNote);
 
 export default router;
