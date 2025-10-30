d92c5b78dd9f76d358e82d27fd2b7cd3f6986339
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-10-30 00:46:29 +0000

    ch

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { upload } from "../middlewares/multer.js";
+import { upload } from "../middleware/multer.js";
 import { updateUserAvatar } from "../controllers/userController.js";
 
 const router = express.Router();
